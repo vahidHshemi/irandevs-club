67a51541a459004ce57794107bb45862855f4f28
vahid hashemi <vahidhashemi73@gmail.com>
2022-03-02 14:26:57 +0330

    Procfile created!

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -196,6 +196,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
